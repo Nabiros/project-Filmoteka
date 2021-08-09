a03b0dd0372fccc1157e754756b79e2d0cd0bbbc
RaiderTs <63519514+RaiderTs@users.noreply.github.com>
2021-08-10 01:03:45 +0300

    Добавляет рендер главной страницы + кнопка вверх + API

--- src/js/apiServise.js
+++ src/js/apiServise.js
@@ -1,3 +1,60 @@
-import axios from "axios";
+const BASE_URL = `https://api.themoviedb.org/3`;
+const KEY = `98821d28938ee5f201a6b9b7afe95fef`;
+export default class NewApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  fetchPopularMovie() {
+    const url = `${BASE_URL}/movie/popular?api_key=${KEY}&language=en-US&page=${this.page}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ results }) => {
+        return results;
+      });
+  }
+  fetchSearchMovie() {
+    const url = `${BASE_URL}/search/movie?api_key=${KEY}&language=en-US&page=${this.page}&query=${this.searchQuery}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ results }) => {
+        return results;
+      });
+  }
+ 
+  fetchGenres() {
+    const url = `${BASE_URL}/genre/movie/list?api_key=${KEY}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        return data.genres;
+      });
+  }
 
+  addGenresToMovieObj() {
+    return this.fetchPopularMovie().then(data => {
+      return this.fetchGenres().then(genresList => {
+        return data.map(movie => ({
+          ...movie,
+          release_date: movie.release_date.split('-')[0],
+          genres: movie.genre_ids
+            .map(id => genresList.filter(el => el.id === id))
+            .flat(),
+        }));
+      });
+    });
+  }
 
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+  get pageNum() {
+    return this.page;
+  }
+  set pageNum(newPage) {
+    this.page = newPage;
+  }
+}
