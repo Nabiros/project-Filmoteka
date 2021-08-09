690e4684f2bfa8763db8d4bb7aa7a2cf26d6da83
Natalia Sorokina <pticasonabird@gmail.com>
2021-08-09 17:19:37 +0300

    removed unnecessary comments

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,16 +1,3 @@
-// (() => {
-//   const refs = {
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//   };
-
-//   refs.closeModalBtn.addEventListener('click', closeModal);
-
-//   function closeModal() {
-//     refs.modal.classList.add('visually-hidden');
-//   }
-// })();
-
 const closeModalBtn = document.querySelector('[data-modal-close]');
 const modal = document.querySelector('[data-modal]');
 
