ad5a1c571fdb205e72e0c46621ef6a20ec8d747e
Maria-Tkh <mcweaver117@gmail.com>
2021-08-18 14:29:00 +0300

    Update genres.js

--- src/js/genres.js
+++ src/js/genres.js
@@ -25,19 +25,19 @@ genresInput.addEventListener('click', onGenresListSearch);
 function onGenresListSearch(e) {
     e.preventDefault();
     genresList.innerHTML = '';
-     renderGenresList(genres);
+     renderGenresList(genres.name);
 }
 
-function renderGenresList(genre) {
+function renderGenresList() {
     newApiService.fetchByGenres();
         
-    const genresListMarkup = createGenresListMarkup(genre);
+    const genresListMarkup = createGenresListMarkup(genres.name);
     genresList.innerHTML = ('beforeend', genresListMarkup);
     console.log(genresList.innerHTML);
 }
 
 function createGenresListMarkup(genres) {
-    return genres.map(genre => `<li class = "genre-title">${genre}</li>`).join('');
+    return genres.map(genre => `<li class = "genre-title">${genres.name}</li>`).join('');
 }
 
 
@@ -45,68 +45,64 @@ function createGenresListMarkup(genres) {
 
 // const genresInput = document.querySelector('.genres-input');
 // const genresList = document.querySelector('.genres-list');
-const clickedGenreTitle = document.querySelector('.genre-title');
-const genresInputField = document.querySelector('.genres-input__field');
+// const clickedGenreTitle = document.querySelector('.genre-title');
+// const genresInputField = document.querySelector('.genres-input__field');
 
-genresInputField.addEventListener('submit', onSearchMoviesByGenre)
-clickedGenreTitle.addEventListener('click', onSearchByGenre);
+// genresInputField.addEventListener('submit', onSearchMoviesByGenre)
+// clickedGenreTitle.addEventListener('click', onSearchByGenre);
 
-function onSearchByGenre(e) {
-    clickedGenreTitle = e.target.value;
-    console.log(e.target.value);
-  genresInputField.value = clickedGenreTitle.textContent;
+// function onSearchByGenre(e) {
+//     const clickedGenreTitle = e.target.value;
+//     // console.log(e.target.value);
+//   genresInputField.value = clickedGenreTitle.textContent;
 
-  if (genresInputValue.length === 0) {
-    popularMovieRender();
-  } else {
-    newApiService.searchQuery = genresInputValue;
-    genreRenderOfMovie();
-  }
-}
+//    newApiService.searchQuery = genresInputValue;
+//     genreRenderOfMovie();
+//   }
 
-function genreRenderOfMovie() {
-  newApiService
-    .fetchByGenreBtn(pagination.getCurrentPage())
-    .then(data => {
-      pagination.reset(data.total_pages);
-      renderMovie(dateAndGenreNormalization(data));
-    })
-    .catch(err => {
-      console.log('error in function render');
-      listElement.innerHTML = `<img  src="${errorUrl}" />`;
-    });
-}
+// function genreRenderOfMovie() {
+//   newApiService
+//     .fetchByGenreBtn(pagination.getCurrentPage())
+//     .then(data => {
+//       pagination.reset(data.total_pages);
+//       renderMovie(dateAndGenreNormalization(data));
+//     })
+//     .catch(err => {
+//       console.log('error in function render');
+//       listElement.innerHTML = `<img  src="${errorUrl}" />`;
+//     });
+// }
 
 
-function appendMoviesMarkup(movies) {
-    listElement.insertAdjacentHTML('beforeend', movieCards(movies));
-}
+// function appendMoviesMarkup(movies) {
+//     listElement.insertAdjacentHTML('beforeend', movieCards(movies));
+// }
 
-function clearCardCollection() {
-    listElement.innerHTML = '';
+// function clearCardCollection() {
+//     listElement.innerHTML = '';
     
-}
+// }
 
-function onSearchMoviesByGenre(e) {
-    e.preventDefault();
-    newApiService.searchQuery = e.currentTarget.elements.searchQuery.value.trim();
-    clearCardCollection();
-    if (newApiService.searchQuery === '') {
-        return;
-    }
-    fetchByGenres();
-}
+// function onSearchMoviesByGenre(e) {
+//     e.preventDefault();
+//     newApiService.searchQuery = e.currentTarget.elements.searchQuery.value.trim();
+//     clearCardCollection();
+//     if (newApiService.searchQuery === '') {
+//         return;
+//     }
+//     fetchByGenres();
+// }
   
-function fetchByGenres() {
-    newApiService.fetchByGenres().then(movies => {
-        appendMoviesMarkup(movies);
+// function fetchByGenres() {
+//     newApiService.fetchByGenres().then(movies => {
+//         appendMoviesMarkup(movies);
        
-    })
-        .catch(err => {
-    console.log('error in function render');
-    listElement.innerHTML = `<img  src="${errorUrl}" />`;
-  });
-}
+//     })
+//         .catch(err => {
+//     console.log('error in function render');
+//     listElement.innerHTML = `<img  src="${errorUrl}" />`;
+//   });
+// }
 
 
 
