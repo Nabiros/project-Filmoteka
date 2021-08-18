17c9c9978664ca131d684cbb43e7080b9ce8d390
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-08-18 21:33:03 +0300

    fix

--- src/js/newLocalStorage.js
+++ src/js/newLocalStorage.js
@@ -4,8 +4,8 @@ import NewApiService from './apiServise';
 
 const newApiService = new NewApiService();
 
-export const watchedLibrary = JSON.parse(localStorage.getItem('watchedLibrary'));
-export const queueLibrary = JSON.parse(localStorage.getItem('queueLibrary'));
+export const watchedLibrary = JSON.parse(localStorage.getItem('watchedLibrary')) || [];
+export const queueLibrary = JSON.parse(localStorage.getItem('queueLibrary')) || [];
 
 export const onWatchedLibraryBtnClick = (e) => {
     if (e.target.className === 'watched-library-button') {
