4978c034b8d0d03a16bfde75f083f03fb7f3566c
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-08-19 19:00:18 +0300

    fix notify

--- src/js/buttonUp.js
+++ src/js/buttonUp.js
@@ -10,7 +10,7 @@ let scroll = 0;
 
 window.addEventListener('scroll', function (e) {
   scroll = window.scrollY;
-  if (scroll > 0) {
+  if (scroll > 500) {
     btnUp.classList.remove('visually-hidden');
   }
   if (scroll === 0) {
--- src/js/renderWithInput.js
+++ src/js/renderWithInput.js
@@ -1,5 +1,6 @@
 import { headerInputField, paginationContainer, listElement } from '../js/refs';
 import errorUrl from '../images/something_went_wrong.webp';
+import notFoundImg from '../images/404error.png';
 import NewApiService from './apiServise';
 import Pagination from 'tui-pagination';
 import {
@@ -10,6 +11,7 @@ import {
 } from './paginationRender';
 import { scrollPage } from './buttonUp.js';
 import { debounce } from 'lodash';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const newApiService = new NewApiService();
@@ -18,26 +20,43 @@ const pagination = new Pagination(paginationContainer, options);
 headerInputField.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(e) {
-  const inputValue = e.target.value.trim();
 
+    const inputValue = e.target.value.trim();
   if (inputValue.length === 0) {
     popularMovieRender();
-  } else {
+      paginationContainer.style.display = 'block';
+
+  }
+  else {
     newApiService.searchQuery = inputValue;
     firstRenderOfMovie();
   }
+  
 }
 
 function firstRenderOfMovie() {
   newApiService
     .fetchByInputValue(pagination.getCurrentPage())
-    .then(data => {
+    .then(data => {      
+      if (data.total_results === 0) {
+        Notiflix.Notify.failure('Sorry, there are no movies matching your search query. Please try again.');
+        paginationContainer.style.display = 'none';
+        throw new Error(data.status);
+        
+      }
+      console.log(data);
+
       pagination.reset(data.total_pages);
       renderMovie(dateAndGenreNormalization(data));
+      paginationContainer.style.display = 'block';
+
+      
     })
-    .catch(err => {
+    
+
+    .catch(err => {      
       console.log('error in function render');
-      listElement.innerHTML = `<img  src="${errorUrl}" />`;
+      listElement.innerHTML = `<img  src="${notFoundImg}" />`;
     });
 }
 
